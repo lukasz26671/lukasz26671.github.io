7b6cac81b92052d7a3ad25e66bd5c0f940d72a02
lukasz26671 <43091565+lukasz26671@users.noreply.github.com>
2018-09-12 19:12:02 +0200

    Update

--- Skrypty/Pierwiastkowanie i Potegowanie.js
+++ Skrypty/Pierwiastkowanie i Potegowanie.js
@@ -1,13 +1,38 @@
 
 function Pierwiastkowanie(liczba) {	
 	alert("Wybrałeś/aś pierwiastkowanie!");
-	liczba = prompt("Wpisz liczbę", "liczba");
 	
-	Wynik = Math.sqrt(liczba);
+		alert("Uwaga! Na razie maksymalny stopień pierwiastka to 3!");
+	stopien = prompt("Wpisz stopień", "stopień");
+
+	liczba = prompt("Wpisz liczbę", "liczba");	
 	
-	alert(Wynik);
 	
-	document.getElementById("Dzialanie").innerHTML = "&#8730;" + liczba + " = " + Wynik;
+	if (stopien == 1) {
+			alert("Pierwiastek 1 stopnia jest taki sam jak liczba, którą chce się spierwiastkować!");
+			return liczba;
+			document.getElementById("Dzialanie").innerHTML = "&#8730;" + liczba + " = " + liczba;
+	} else if (stopien == 2) {
+		
+		
+		} if (liczba >= 0) {
+			Wynik = Math.sqrt(liczba);
+			alert(Wynik);
+			document.getElementById("Dzialanie").innerHTML = "&#8730;" + liczba + " = " + Wynik;
+		} else if (liczba < 0) {
+			alert("Pierwiastek kwadratowy nie może być liczbą ujemną!")
+			document.getElementById("Dzialanie").innerHTML = "&#8730;" + liczba + " = " + "niemożliwe"
+		
+	} else if (stopien == 3) {
+			Wynik = Math.cbrt(liczba);
+			alert(Wynik);
+			document.getElementById("Dzialanie").innerHTML = "&#8731;" + liczba + " = " + Wynik;
+	} else {
+		return null;
+		alert(null);
+		document.getElementById("Dzialanie").innerHTML = "Nie jest zaimplementowane stopniowanie pierwiastków powyżej stopnia 3!"
+	};
+	
 };
 
 function Potegowanie(liczba, wykladnik) {
@@ -21,3 +46,5 @@ function Potegowanie(liczba, wykladnik) {
 	
 	document.getElementById("Dzialanie").innerHTML =  liczba + "^" + wykladnik + " = " + Wynik;
 };
+
+
