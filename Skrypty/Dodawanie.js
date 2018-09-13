a62652b3a937c0b5c4888de8a7efa08f6b586f8c
lukasz26671 <43091565+lukasz26671@users.noreply.github.com>
2018-09-13 16:33:45 +0200

    Add copyright

--- Skrypty/Dodawanie.js
+++ Skrypty/Dodawanie.js
@@ -1,3 +1,4 @@
+//Copyright Â© 2018 lukasz26671
 /*
   Skrypt Dodawania by Lukasz26671 
         (Wersja PL)
@@ -5,14 +6,14 @@
 
 function Dodawanie() { //Funkcja dodawania
 
-    alert("Wybrales/as dodawanie!"); //Powiadomienie o typie dzia³ania
+    alert("Wybrales/as dodawanie!"); //Powiadomienie o typie dziaÅ‚ania
 
-    Liczba1 = prompt("Wpisz pierwsza liczbe", "Liczba"); //Wpisz wartoœæ liczby 1
-    Liczba2 = prompt("Wpisz druga liczbe", "Liczba"); //Wpisz wartoœæ liczby 2
+    Liczba1 = prompt("Wpisz pierwsza liczbe", "Liczba"); //Wpisz wartoÅ›Ä‡ liczby 1
+    Liczba2 = prompt("Wpisz druga liczbe", "Liczba"); //Wpisz wartoÅ›Ä‡ liczby 2
     Wynik = +Liczba1 + +Liczba2; //Zmienna Wynik
 
-    alert(Liczba1 + "+" + Liczba2 + "=" + Wynik); //Ostrze¿ o wyniku
+    alert(Liczba1 + "+" + Liczba2 + "=" + Wynik); //OstrzeÅ¼ o wyniku
 
-         document.getElementById("Dzialanie").innerHTML = Liczba1 + " + " + Liczba2 + " = " + Wynik; //Napisz na stronie dzia³anie i wynik
+         document.getElementById("Dzialanie").innerHTML = Liczba1 + " + " + Liczba2 + " = " + Wynik; //Napisz na stronie dziaÅ‚anie i wynik
 
 }
