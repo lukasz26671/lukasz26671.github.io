bb1fbbe52558e2e0ae9792be9cd207257e30a15b
lukasz26671 <43091565+lukasz26671@users.noreply.github.com>
2018-09-13 16:34:08 +0200

    Add copyright

--- Skrypty/Dzielenie.js
+++ Skrypty/Dzielenie.js
@@ -1,3 +1,4 @@
+//Copyright Â© 2018 lukasz26671
 /*
   Skrypt Dzielenia by Lukasz26671 
         (Wersja PL)
@@ -7,21 +8,21 @@ function Dzielenie() { //funkcja Dzielenia
 
     alert("Wybrales/as dzielenie"); //Powiadomienie o typie dzialania
 
-    Liczba1 = prompt("Wpisz pierwsza liczbe", "Liczba"); //Wpisz wartoœæ liczby 1 (dzielna)
-    Liczba2 = prompt("Wpisz druga liczbe", "Liczba"); //Wpisz wartoœæ liczby 2 (dzielnik)
+    Liczba1 = prompt("Wpisz pierwsza liczbe", "Liczba"); //Wpisz wartoÅ›Ä‡ liczby 1 (dzielna)
+    Liczba2 = prompt("Wpisz druga liczbe", "Liczba"); //Wpisz wartoÅ›Ä‡ liczby 2 (dzielnik)
 
-    if (Liczba2 == 0) { //Je¿eli dzielnik jest równy 0, wtedy:
+    if (Liczba2 == 0) { //JeÅ¼eli dzielnik jest rÃ³wny 0, wtedy:
 
-        alert("Nie mozna dzielic przez 0!"); //Ostrze¿ "Nie mo¿na dzieliæ przez 0!"
-        document.getElementById("Dzialanie").innerHTML = "Nie mozna dzielic przez 0!"; //Napisz na stronie "Nie mo¿na dzieliæ przez 0!"
+        alert("Nie mozna dzielic przez 0!"); //OstrzeÅ¼ "Nie moÅ¼na dzieliÄ‡ przez 0!"
+        document.getElementById("Dzialanie").innerHTML = "Nie mozna dzielic przez 0!"; //Napisz na stronie "Nie moÅ¼na dzieliÄ‡ przez 0!"
 
     } else { //W innym wypadku:
 
         Wynik = +Liczba1 / +Liczba2; //Zmienna wynik
 
-        alert(+Liczba1 + "/" + Liczba2 + "=" + Wynik); //Ostrze¿ o wyniku
+        alert(+Liczba1 + "/" + Liczba2 + "=" + Wynik); //OstrzeÅ¼ o wyniku
 
-        document.getElementById("Dzialanie").innerHTML = Liczba1 + " / " + Liczba2 + "=" + Wynik; //Napisz na stronie dzia³anie i wynik
+        document.getElementById("Dzialanie").innerHTML = Liczba1 + " / " + Liczba2 + "=" + Wynik; //Napisz na stronie dziaÅ‚anie i wynik
     }
 
 };
