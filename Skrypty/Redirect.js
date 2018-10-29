3ddc91f7bfb024ae673594d60d82d0e6cfc16227
lukasz26671 <43091565+lukasz26671@users.noreply.github.com>
2018-10-29 20:29:35 +0100

    Add files via upload

--- Skrypty/Redirect.js
+++ Skrypty/Redirect.js
@@ -1,27 +1,48 @@
-//Skrypt sterujacy przyciskami by Lukasz26671  
-//Copyright © 2018 lukasz26671
+//Skrypt sterujacy przyciskami by Lukasz26671
 
 /*
 
 Powiadomienia = document.getElementById("Powiadomienia").checked; ----WYŁĄCZONE Z POWODU BRAKU ZASTOSOWANIA
 
 */
-function redirectBack() { //funkcja przekieruj spowrotem do strony 1
-  	setTimeout("location.href='index.html';", 50);
-};
 
-function redirectMain() { //funkcja przekieruj do strony 2
-  	setTimeout("location.href='podstrona.html';", 50);
-};
 
-function redirectMain2() { //funkcja przekieruj do strony 3
-  	setTimeout("location.href='kalkulator.html';", 50);
-};
+	
+	function redirectBack() { //funkcja przekieruj spowrotem do strony 1
+		setTimeout("location.href='index.html';", 50);
+	};
 
-function redirectMain3() { //funkcja przekieruj do strony 4
-  	setTimeout("location.href='podstrona2.html';", 50);
-};
+	function redirectMain() { //funkcja przekieruj do strony 3
+		setTimeout("location.href='podstrona.html';", 50);
+	};
 
-function redirectSchl() { //funkcja przekieruj do strony 4
-  	setTimeout("window.open('http://spkg.pl', '_blank');", 50);
-};
+	function redirectMain2() { //funkcja przekieruj do strony 2
+		setTimeout("location.href='kalkulator.html';", 50);
+	};
+
+	function redirectMain3() { //funkcja przekieruj do strony 4
+		setTimeout("location.href='podstrona2.html';", 50);
+	};
+	function redirectSchl() { //funkcja przekieruj do strony 4
+		setTimeout("window.open('http://spkg.pl', '_blank');", 50);
+	};
+	function redirectChVid() { //funkcja przekieruj do Wybor video
+		setTimeout("location.href='Wybor video.html';", 50);
+	};
+	function redirectVid1() { //funkcja przekieruj do video1
+		setTimeout("location.href='Video01.html';", 50);
+	};
+	function redirectVid2() { //funkcja przekieruj do video2
+		setTimeout("location.href='Video02.html';", 50);
+	};
+	function redirectVid3() { //funkcja przekieruj do video3
+		setTimeout("location.href='Video03.html';", 50);
+	};
+	function redirectVid4() { //funkcja przekieruj do video3
+		setTimeout("location.href='Video04.html';", 50);
+	};
+
+	function redirectChn() {
+		setTimeout("window.open('https://www.youtube.com/channel/UCWTjTkGZCSf5-XEZKUMQxoQ?view_as=subscriber', '_blank');", 50);
+	};
+
