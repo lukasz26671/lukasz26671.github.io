9c223ae48d74ab610237768f4c696ccc35ff7706
lukasz26671 <43091565+lukasz26671@users.noreply.github.com>
2018-10-30 18:29:44 +0100

    Update Redirect.js

--- Skrypty/Redirect.js
+++ Skrypty/Redirect.js
@@ -9,37 +9,35 @@ Powiadomienia = document.getElementById("Powiadomienia").checked; ----WYŁĄCZON
 
 	
 	function redirectBack() { //funkcja przekieruj spowrotem do strony 1
-		setTimeout("location.href='index.html';", 50);
+		setTimeout("location.href='index';", 50);
 	};
 
 	function redirectMain() { //funkcja przekieruj do strony 3
-		setTimeout("location.href='podstrona.html';", 50);
+		setTimeout("location.href='podstrona';", 50);
 	};
 
 	function redirectMain2() { //funkcja przekieruj do strony 2
-		setTimeout("location.href='kalkulator.html';", 50);
+		setTimeout("location.href='kalkulator';", 50);
 	};
 
 	function redirectMain3() { //funkcja przekieruj do strony 4
-		setTimeout("location.href='podstrona2.html';", 50);
+		setTimeout("location.href='podstrona2';", 50);
 	};
-	function redirectSchl() { //funkcja przekieruj do strony 4
-		setTimeout("window.open('http://spkg.pl', '_blank');", 50);
-	};
+
 	function redirectChVid() { //funkcja przekieruj do Wybor video
-		setTimeout("location.href='Wybor video.html';", 50);
+		setTimeout("location.href='Wybor video';", 50);
 	};
 	function redirectVid1() { //funkcja przekieruj do video1
-		setTimeout("location.href='Video01.html';", 50);
+		setTimeout("location.href='Video01';", 50);
 	};
 	function redirectVid2() { //funkcja przekieruj do video2
-		setTimeout("location.href='Video02.html';", 50);
+		setTimeout("location.href='Video02';", 50);
 	};
 	function redirectVid3() { //funkcja przekieruj do video3
-		setTimeout("location.href='Video03.html';", 50);
+		setTimeout("location.href='Video03';", 50);
 	};
 	function redirectVid4() { //funkcja przekieruj do video3
-		setTimeout("location.href='Video04.html';", 50);
+		setTimeout("location.href='Video04';", 50);
 	};
 
 	function redirectChn() {
