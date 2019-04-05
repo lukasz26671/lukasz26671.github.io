1811f65c7a0b4dc6f94b70f451e2e192065f0a8b
Lukasz <43091565+lukasz26671@users.noreply.github.com>
2019-04-05 23:56:35 +0200

    general update xDDDD

---
+++ Skrypty/Dzialania.js
@@ -0,0 +1,77 @@
+
+	function pokazDzialanie(tekst) {
+	  var newDiv = document.createElement("div"); 
+	  var newContent = document.createTextNode(tekst); 
+
+	  newDiv.appendChild(newContent);  
+
+	  var currentDiv = document.getElementById("div1"); 
+	  document.body.insertBefore(newDiv, currentDiv); 
+	}
+
+	function Dodawanie() { //Funkcja dodawania
+
+		alert("Wybrales/as dodawanie!"); //Powiadomienie o typie działania
+
+		Liczba1 = prompt("Wpisz pierwsza liczbe", "Liczba"); //Wpisz wartość liczby 1
+		Liczba2 = prompt("Wpisz druga liczbe", "Liczba"); //Wpisz wartość liczby 2
+		Wynik = +Liczba1 + +Liczba2; //Zmienna Wynik
+
+		alert(Liczba1 + "+" + Liczba2 + "=" + Wynik); //Ostrzeż o wyniku
+
+			pokazDzialanie(Liczba1 + "+" + Liczba2 + "=" + Wynik);
+
+	}
+
+	function Odejmowanie() { //Funkcja odejmowania
+
+		alert("Wybrales/as odejmowanie!"); //Powiadomienie o typie działania
+
+		Liczba1 = prompt("Wpisz pierwsza liczbe", "Liczba"); //Wpisz wartość liczby 1
+		Liczba2 = prompt("Wpisz druga liczbe", "Liczba"); //Wpisz wartość liczby 2
+
+		Wynik = +Liczba1 - +Liczba2; //Zmienna wynik
+
+		alert(Liczba1 + "-" + Liczba2 + "=" + Wynik); //Ostrzeż o wyniku
+
+			pokazDzialanie(Liczba1 + "-" + Liczba2 + "=" + Wynik);
+	}
+	
+	function Mnozenie() { //Funkcja mnożenia
+
+		alert("Wybrales/as mnozenie!"); //Powiadomienie o typie działania
+
+		Liczba1 = prompt("Wpisz pierwsza liczbe", "Liczba"); //Wpisz wartość liczby 1
+		Liczba2 = prompt("Wpisz druga liczbe", "Liczba"); //Wpisz wartość liczby 2
+
+		Wynik = +Liczba1 * +Liczba2; //Zmienna wynik
+
+		alert(Liczba1 + "*" + Liczba2 + "=" + Wynik); //Ostrzeż o wyniku
+
+			pokazDzialanie(Liczba1 + "*" + Liczba2 + "=" + Wynik);
+
+	}	
+	
+	function Dzielenie() { //funkcja Dzielenia
+
+		alert("Wybrales/as dzielenie"); //Powiadomienie o typie dzialania
+
+		Liczba1 = prompt("Wpisz pierwsza liczbe", "Liczba"); //Wpisz wartość liczby 1 (dzielna)
+		Liczba2 = prompt("Wpisz druga liczbe", "Liczba"); //Wpisz wartość liczby 2 (dzielnik)
+
+		if (Liczba2 == 0) { //Jeżeli dzielnik jest równy 0, wtedy:
+
+			alert("Nie mozna dzielic przez 0!"); //Ostrzeż "Nie można dzielić przez 0!"
+					pokazDzialanie("Nie mozna dzielic przez 0!");
+
+
+		} else { //W innym wypadku:
+
+			Wynik = +Liczba1 / +Liczba2; //Zmienna wynik
+
+			alert(+Liczba1 + "/" + Liczba2 + "=" + Wynik); //Ostrzeż o wyniku
+
+					pokazDzialanie(Liczba1 + "/" + Liczba2 + "=" + Wynik);
+
+		}	
+	};
