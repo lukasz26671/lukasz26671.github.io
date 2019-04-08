1c4f5672a95108a43fcdab6f741842532ab249b9
Lukasz <43091565+lukasz26671@users.noreply.github.com>
2019-04-08 21:14:31 +0200

    update lol
    yeet

--- Skrypty/Redirect.js
+++ Skrypty/Redirect.js
@@ -17,29 +17,17 @@ Powiadomienia = document.getElementById("Powiadomienia").checked; ----WYŁĄCZON
 	};
 
 	function redirectMain3() { //funkcja przekieruj do strony 4
-		setTimeout("location.href='podstrona2.html';", 50);
-	};
-
-	function redirectChVid() { //funkcja przekieruj do Wybor video
-		setTimeout("location.href='Wybor video.html';", 50);
-	};
-	function redirectVid1() { //funkcja przekieruj do video1
-		setTimeout("location.href='Video01.html';", 50);
-	};
-	function redirectVid2() { //funkcja przekieruj do video2
-		setTimeout("location.href='Video02.html';", 50);
-	};
-	function redirectVid3() { //funkcja przekieruj do video3
-		setTimeout("location.href='Video03.html';", 50);
-	};
-	function redirectVid4() { //funkcja przekieruj do video3
-		setTimeout("location.href='Video04.html';", 50);
+		setTimeout("location.href='zrodla.html';", 50);
 	};
 
 	function redirectMcMod() { //funkcja przekieruj do video3
 		setTimeout("location.href='mcmod.html';", 50);
 	};
 
+	function redirectContact() { //funkcja przekieruj do video3
+		setTimeout("location.href='kontakt.html';", 50);
+	};
+	
 	function redirectMod() {
 		setTimeout("window.open('https://github.com/lukasz26671/Lukasz26671Utils/releases', '_blank');", 50);
 	};
