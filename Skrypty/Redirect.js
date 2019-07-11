5ccc87112ed5bc4ccefe4271a8ba9bf39abf3e55
Lukasz <43091565+lukasz26671@users.noreply.github.com>
2019-07-11 19:34:49 +0200

    Update Redirect.js

--- Skrypty/Redirect.js
+++ Skrypty/Redirect.js
@@ -9,27 +9,7 @@
 	};
 
 	function redirectMain3() { //funkcja przekieruj do strony 4
-<<<<<<< HEAD
 		setTimeout("location.href='zrodla.html';", 50);
-=======
-		setTimeout("location.href='podstrona2';", 50);
-	};
-
-	function redirectChVid() { //funkcja przekieruj do Wybor video
-		setTimeout("location.href='Wybor video';", 50);
-	};
-	function redirectVid1() { //funkcja przekieruj do video1
-		setTimeout("location.href='Video01';", 50);
-	};
-	function redirectVid2() { //funkcja przekieruj do video2
-		setTimeout("location.href='Video02';", 50);
-	};
-	function redirectVid3() { //funkcja przekieruj do video3
-		setTimeout("location.href='Video03';", 50);
-	};
-	function redirectVid4() { //funkcja przekieruj do video3
-		setTimeout("location.href='Video04';", 50);
->>>>>>> parent of 1811f65... general update xDDDD
 	};
 
 	function redirectMcMod() { //funkcja przekieruj do video3
