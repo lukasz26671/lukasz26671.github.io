d314bd042c6f8aa173f7cf6bbaebf3851cc81a66
Lukasz <43091565+lukasz26671@users.noreply.github.com>
2019-07-11 19:27:47 +0200

    Add files via upload

--- Skrypty/Redirect.js
+++ Skrypty/Redirect.js
@@ -1,23 +1,43 @@
 //Skrypt sterujacy przyciskami by Lukasz26671
 
 	function redirectBack() { //funkcja przekieruj spowrotem do strony 1
-		setTimeout("location.href='index.html';", 50);
+		setTimeout("location.href='index';", 50);
 	};
 
 	function redirectMain2() { //funkcja przekieruj do strony 2
-		setTimeout("location.href='kalkulator.html';", 50);
+		setTimeout("location.href='kalkulator';", 50);
 	};
 
 	function redirectMain3() { //funkcja przekieruj do strony 4
+<<<<<<< HEAD
 		setTimeout("location.href='zrodla.html';", 50);
+=======
+		setTimeout("location.href='podstrona2';", 50);
+	};
+
+	function redirectChVid() { //funkcja przekieruj do Wybor video
+		setTimeout("location.href='Wybor video';", 50);
+	};
+	function redirectVid1() { //funkcja przekieruj do video1
+		setTimeout("location.href='Video01';", 50);
+	};
+	function redirectVid2() { //funkcja przekieruj do video2
+		setTimeout("location.href='Video02';", 50);
+	};
+	function redirectVid3() { //funkcja przekieruj do video3
+		setTimeout("location.href='Video03';", 50);
+	};
+	function redirectVid4() { //funkcja przekieruj do video3
+		setTimeout("location.href='Video04';", 50);
+>>>>>>> parent of 1811f65... general update xDDDD
 	};
 
 	function redirectMcMod() { //funkcja przekieruj do video3
 		setTimeout("location.href='mcmod.html';", 50);
 	};
 
-	function redirectMod() {
-		setTimeout("window.open('https://github.com/lukasz26671/Lukasz26671Utils/releases', '_blank');", 50);
+	function redirectChn() {
+		setTimeout("window.open('https://www.youtube.com/channel/UCWTjTkGZCSf5-XEZKUMQxoQ?view_as=subscriber', '_blank');", 50);
 	};
 
 	function redirectContact() { //funkcja przekieruj do kontakt
--- Skrypty/main.js
+++ Skrypty/main.js
@@ -1,4 +1,6 @@
 
+const ytapi = ``;
+
 function setCookie(cname, cvalue, exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
